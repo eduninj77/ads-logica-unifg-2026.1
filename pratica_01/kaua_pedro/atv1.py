7a50961ee3416e308511f629a9569bc99a7015a3
--local <--local>
2026-05-19 20:36:32 -0300

    exercicio 1

---
+++ pratica_01/kaua_pedro/atv1.py
@@ -0,0 +1,5 @@
+nome = str(input("escreva seu nome:"))
+ano_nascimento= int(input("escreva seu ano de nascimento:"))
+altura = float(input("escreva sua altura:"))
+
+print("seu nome é, ", nome,"sua idade é,",2026-ano_nascimento,"sua altura é, ", altura,)
